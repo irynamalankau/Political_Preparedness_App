923baf15e401653076656c76f2851ccb210cd092
ira <ira@ira>
2021-10-21 10:14:35 -0500

    implement elections viewmodel

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -2,16 +2,34 @@ package com.example.android.politicalpreparedness.election
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.politicalpreparedness.database.ElectionDatabase
+import kotlinx.coroutines.launch
 
 
-class ElectionsViewModel(application: Application) : AndroidViewModel(application) {
+class ElectionsViewModel(application: Application) : ViewModel() {
 
-    //TODO: Create live data val for upcoming elections
+    //Create database instance
+    private val database = ElectionDatabase.getInstance(application)
+
+    //Create repository instance
+    private val repository = ElectionsRepository(database)
+
+    //Create live data val for upcoming elections
+    private val upcomingElections: LiveData<List<Election>>
+        get() = repository.getCachedElections()
 
     //TODO: Create live data val for saved elections
 
-    //TODO: Create val and functions to populate live data for upcoming elections from the API and saved elections from local database
+    //Create val and functions to populate live data for upcoming elections from the API and saved elections from local database
+    // Create an init block and launch a coroutine to call repository.refreshElectionsList()
+    init{
+        viewModelScope.launch {
+            repository.refreshElectionsList()
+        }
+    }
 
     //TODO: Create functions to navigate to saved or upcoming election voter info
 
