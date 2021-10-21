e00278619f622043aed9c53f469dba8409bec13f
ira <ira@ira>
2021-10-21 16:20:12 -0500

    enable UP button

--- app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.android.politicalpreparedness
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.politicalpreparedness.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding: ActivityMainBinding =
+                DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        val navController = navHostFragment?.findNavController()
+        if (navController != null) {
+            NavigationUI.setupActionBarWithNavController(this, navController)
+        }
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
