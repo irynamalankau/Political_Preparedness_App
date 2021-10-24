c297dbe0d8932a58c00da0955df48ca100c470ca
ira <ira@ira>
2021-10-24 11:51:32 -0500

    add model

--- app/src/main/java/com/example/android/politicalpreparedness/network/models/VoterInfoResponse.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/models/VoterInfoResponse.kt
@@ -5,8 +5,10 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 class VoterInfoResponse (
         val election: Election,
-        /*val pollingLocations: String? = null, //TODO: Future Use
-        val contests: String? = null, //TODO: Future Use*/
+        //val pollingLocations: String? = null, //TODO: Future Use
+        //val contests: String? = null, //TODO: Future Use*/
         val state: List<State>? = null,
-        val electionElectionOfficials: List<ElectionOfficial>? = null
+        val kind: String
+        //val electionElectionOfficials: List<ElectionOfficial>? = null
+
 )
